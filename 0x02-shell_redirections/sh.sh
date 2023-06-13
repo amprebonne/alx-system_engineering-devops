3899f6e40fc787f7a126222c144d4699391b5f38
amprebonne <amprebonne@gmail.com>
2023-06-12 18:34:23 -0700

    added readme

--- 0x02-shell_redirections/sh.sh
+++ 0x02-shell_redirections/sh.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
-# Step 1: Receive two arguments
-arg1=$1
-arg2=$2
+# Receive three arguments
+first_arg="$1"
+second_arg="$2"
+third_arg="$3"
 
-# Step 2: Create a file in the current working directory using Vim named after the first argument
-file_path=$(pwd)/$arg1
-vim $file_path
+# Create a file in the current working directory using Vim named after the first argument
+vim "${first_arg}"
 
-# Step 3: Write "#!/bin/bash" and the second argument as the first and second lines to the file
-echo "#!/bin/bash" > $file_path
-echo $arg2 >> $file_path
+# Inside Vim, write "#!/bin/bash" and the second argument as the first and second lines
+echo -e "#!/bin/bash\n${second_arg}" >> "${first_arg}"
+# Press ESC key and enter ":wq" to save and exit Vim
 
-# Step 4: Make the file executable
-chmod +x $file_path
+# Make the file executable
+chmod +x "${first_arg}"
 
-# Step 5: Save and exit Vim
-vim -c ":wq"
+# Git add the file
+git add "${first_arg}"
 
-# Step 6: Git add the file
-git add $file_path
+# Git commit with the third argument as the commit message
+git commit -m "${third_arg}"
 
-# Step 7: Git commit with message
-commit_message="added $arg1"
-git commit -m "$commit_message"
+# Git push
+git push
 
-# Step 8: Open the file in Vim
-vim $file_path
