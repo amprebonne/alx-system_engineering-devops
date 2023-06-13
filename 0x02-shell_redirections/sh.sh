9af768a6ebcd8697fe58ad9befd260334b59fb73
amprebonne <amprebonne@gmail.com>
2023-06-13 03:50:46 -0700

    22-users_and_homes

--- 0x02-shell_redirections/sh.sh
+++ 0x02-shell_redirections/sh.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# Receive three arguments
-first_arg="$1"
-second_arg="$2"
-third_arg="$3"
+# Step 1: Receive two arguments
+arg1="$1"
+arg2="$2"
 
-# Create a file in the current working directory using Vim named after the first argument
-vim "${first_arg}"
+# Step 2: Create a file using Vim named after the first argument
+vim "$arg2"
 
-# Inside Vim, write "#!/bin/bash" and the second argument as the first and second lines
-echo -e "#!/bin/bash\n${second_arg}" >> "${first_arg}"
-# Press ESC key and enter ":wq" to save and exit Vim
+# Step 3: Write "#!/bin/bash" as the first line of the file
+echo "#!/bin/bash" > "$arg2"
 
-# Make the file executable
-chmod +x "${first_arg}"
+# Step 4: Open the file in Vim for additional editing
+vim "$arg2"
 
-# Git add the file
-git add "${first_arg}"
+# Step 5: Make the file executable
+chmod +x "$arg2"
 
-# Git commit with the third argument as the commit message
-git commit -m "${third_arg}"
+# Step 6: Git add the file
+git add "$arg2"
 
-# Git push
+# Step 7: Git commit with the second argument as the commit message
+git commit -m "$ar1"
+
+# Step 8: Git push
 git push
 
